f5084a28da158dfe93b308eb1d0652714d4df255
Chetan Sarva <chetan@pixelcop.net>
2014-04-16 10:59:37 -0400

    implemented directory handling

--- lib/bixby/provision/dsl/base.rb
+++ lib/bixby/provision/dsl/base.rb
@@ -15,6 +15,48 @@ module Bixby
         self.instance_eval(&block)
       end
 
+      def get_uid(user)
+        return user if user.nil? or user.kind_of? Fixnum
+        begin
+          return Etc.getpwnam(user).uid
+        rescue ArgumentError => ex
+          # TODO raise
+          logger.warn("Username '#{user}' was invalid: #{ex.message}")
+        end
+        nil
+      end
+
+      def get_user(uid)
+        begin
+          return Etc.getpwuid(uid).name
+        rescue ArgumentError => ex
+          # TODO raise
+          logger.warn("Username '#{user}' was invalid: #{ex.message}")
+        end
+        nil
+      end
+
+      def get_gid(group)
+        return group if group.nil? or group.kind_of? Fixnum
+        begin
+          return Etc.getgrnam(group).gid
+        rescue ArgumentError => ex
+          # TODO raise
+          logger.warn("Group '#{group}' was invalid: #{ex.message}")
+        end
+        nil
+      end
+
+      def get_group(gid)
+        begin
+          return Etc.getgrgid(gid).name
+        rescue ArgumentError => ex
+          # TODO raise
+          logger.warn("Group '#{group}' was invalid: #{ex.message}")
+        end
+        nil
+      end
+
     end
 
   end
--- lib/bixby/provision/dsl/dir.rb
+++ lib/bixby/provision/dsl/dir.rb
@@ -2,14 +2,36 @@
 module Bixby
   module Provision
 
-    class DirDSL
+    class DirDSL < Base
 
       EXPORTS = []
 
       def create(path, opts={})
+        logger.info "ensuring #{path} exists"
+        FileUtils.mkdir_p(path) if !File.exists? path
+        chown(path, opts.delete(:chown))
       end
 
       def recreate(path, opts={})
+      end
+
+      def chown(path, chown, opts={})
+        chown.strip! if !chown.nil?
+        return if chown.empty?
+
+        user, group = chown.split(/:/)
+        user = Process.uid if user == "$USER"
+        group = Process.gid if group == "$GROUP"
+        uid = get_uid(user)
+        gid = get_gid(group)
+
+        logger.info "ensuring ownership matches '#{get_user(uid)}" + (gid ? ":#{get_group(gid)}'" : "'")
+
+        if opts[:recurse] or opts[:recursively] then
+          FileUtils.chown_R(uid, gid, [path])
+        else
+          File.chown(uid, gid, path)
+        end
       end
 
     end
