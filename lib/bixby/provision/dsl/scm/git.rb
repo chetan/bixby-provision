56b86bf44c961e48e3941accd9b245aa6ebc49af
Chetan Sarva <chetan@pixelcop.net>
2014-05-02 18:52:33 -0400

    disable strict host checks during checkout (since we'll be running non-interactively)

--- lib/bixby/provision/dsl/scm/git.rb
+++ lib/bixby/provision/dsl/scm/git.rb
@@ -21,9 +21,63 @@ module Bixby
           end
           dir.create(path)
 
-          logger.info "cloning #{uri} into #{path}, branch: #{branch}"
+          # https://github.com/user/repo.git
+          # git@github.com:user/repo.git
+          # ssh://hg@bitbucket.org/user/repo
+          if uri !~ %r{https?://} && uri =~ %r{^(ssh://)?(.*?@)(.*?)[:/]} then
+            ensure_ssh_verify($3)
+          end
+
+          logger.info "[scm] cloning #{uri} into #{path}, branch: #{branch}"
           g = ::Git.clone(uri, File.basename(path), :path => File.dirname(path))
           g.checkout(branch) if branch != "master"
+        end
+
+
+        private
+
+        # Ensure that StrictHostKeyChecking is disabled for the given host
+        #
+        # @param [String] host
+        def ensure_ssh_verify(host)
+
+          config = File.expand_path("~/.ssh/config")
+          str = ""
+          str = File.read(config) if File.exists? config
+
+          add = str.empty?
+          if !add then
+            # see if it's already disabled for the given host
+            found_host = false
+            found_strict = false
+            str.split(/n/).each do |l|
+
+              if l =~ /^\s*Host (.*?)/ then
+                if found_host then
+                  add = true if !found_strict
+                  found_host = false
+                  next
+                end
+                if $1 == host then
+                  found_host = true
+                end
+
+              elsif found_host && l =~ /StrictHostKeyChecking \s*no/ then
+                found_strict = true
+              end
+            end
+          end
+
+          return if !add
+
+          logger.info "[scm] disabling StrictHostKeyChecking for #{host}"
+          str += "\n"
+          str += "Host #{host}\n"
+          str += "  StrictHostKeyChecking no\n"
+
+          dir.mkdir "~/.ssh", :chmod => "700"
+          File.open(config, 'w'){ |f| f.write(str) }
+          chmod config, "644"
         end
 
       end
